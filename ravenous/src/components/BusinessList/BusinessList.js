45ac20eadf05935247a12daff6096c7c880411ff
Stephen <TF2Man999@gmail.com>
2018-04-24 13:17:55 -0400

    function

--- ravenous/src/components/BusinessList/BusinessList.js
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -6,12 +6,15 @@ class BusinessList extends React.Component {
   render () {
     return (
       <div className="BusinessList">
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
+      {
+      this.BusinessList.map(functionName(business)
+    {
+      return (
+        < Business business = {this.business}/>
+      )
+    });
+      }
+
 </div>
       )
     }
