3e7acc27fe7d3ead90e3167b5afb33211fc81c5f
Stephen <TF2Man999@gmail.com>
2018-04-28 23:37:46 -0400

    part 3, navigation issues and console issues

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -18,9 +18,12 @@ const sortByOptions = {
         location: '',
         sortBy: 'best_match'
       };
+
+      this.handleSortByChange = this.handleSortByChange.bind(this)
+      this.handleTermChange = this.handleTermChange.bind(this)
+      this.handleLocationChange = this.handleLocationChange.bind(this)
+
     }
-
-
 
     renderSortByOptions() {
       return Object.keys(sortByOptions).map(sortByOption => {
@@ -65,8 +68,8 @@ const sortByOptions = {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+          <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
           <a>Let's Go</a>
